a468daa84b6a25dd271bd8185c2b6fcc6d868f2f
Joao <joao.khalaf@gmail.com>
2025-05-28 23:31:20 -0300

    adding types/node

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
+import { useEffect, useRef, useState } from 'react';
+
+type Todo = {
+  id: Number
+  text: String
+  completed: boolean
+}
+
+export const App = () => {
+
+  const [timeLeft, setTimeLeft] = useState(25 * 60)
+  const [isRunning, setIsRunning] = useState(false)
+  const [isBreak, setIsBreak]= useState(false)
+  const [todos, SetTodos] = useState<Todo[]>([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Learn TypeScript", completed: false },
+    { id: 3, text: "Build a Pomodoro Timer", completed: false }
+  ])
+  const [newTodo, setNewTodo] = useState("")
+  const [sessions, setSessions] = useState(0)
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
+
+  useEffect(() => {
+    if( isRunning && timeLeft > 0) {
+      intervalRef.current = setInterval (() => {
+        setTimeLeft((prev) => prev - 1)
+      }, 1000)
+    } else if (timeLeft === 0) {
+    setIsRunning(false)
+      if (!isBreak){
+        setSessions((prev) => prev + 1 )
+        setIsBreak(true)
+        setTimeLeft(5 * 60)
+      } else {
+        setIsBreak(false)
+        setTimeLeft(25 * 60)
+      }
+  } else {
+    if(intervalRef.current){
+      clearInterval(intervalRef.current)
+    }
+  }
+
+  return () => {
+    if(intervalRef.current) {
+      clearInterval(intervalRef.current)
+    }
+  }
+}, [isRunning, timeLeft, isBreak])
 
 
-function App() {
 
   return (
-    <>
-      <div>
-
-      </div>
-   
-    </>
+    <div>App</div>
   )
 }
 
-export default App
+
