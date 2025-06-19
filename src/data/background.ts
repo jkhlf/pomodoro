a4816c09ca1f97560bdc50422c20ae6ba0a04a68
Joao <joao.khalaf@gmail.com>
2025-06-18 23:55:21 -0300

    close to finish

--- src/data/background.ts
+++ src/data/background.ts
@@ -3,7 +3,21 @@ import type { BackgroundImage } from "../types";
 export const backgroundImages: BackgroundImage[] = [
   {
     id:1,
-    url: "",
-    name: "",
+    url: "https://wallhaven.cc/w/o3r2kl",
+    name: "Lofi Bart",
+  },
+  {
+    id:2,
+    url: "https://wallhaven.cc/w/v91qgp",
+    name: "Lofi ",
+  },{
+    id:3,
+    url: "https://wallhaven.cc/w/exo2gr",
+    name: "Lofi ",
+  },
+  {
+    id:4,
+    url: "https://wallhaven.cc/w/vqojj8",
+    name: "Lofi ",
   }
 ]
