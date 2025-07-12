0b04125f7111f323167446810b448535895b59e9
Joao <142253985+jkhlf@users.noreply.github.com>
2025-07-12 04:32:31 -0300

    backgrounds

--- src/data/background.ts
+++ src/data/background.ts
@@ -4,21 +4,21 @@ export const backgroundImages: BackgroundImage[] = [
   {
     id: "1",
     url: "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png",
-    name: "Lofi Room",
+    name: "City",
   },
   {
     id: "2",
-    url: "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png",
-    name: "Lofi Street",
+    url: "https://w.wallhaven.cc/full/1k/wallhaven-1k5793.jpg",
+    name: "Lofi Bart",
   },
   {
     id: "3",
-    url: "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png",
-    name: "Lofi Desk",
+    url: "https://w.wallhaven.cc/full/e7/wallhaven-e787j8.png",
+    name: "Retro",
   },
   {
     id: "4",
-    url: "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png",
-    name: "Lofi Cat",
+    url: "https://w.wallhaven.cc/full/zp/wallhaven-zpx7lv.jpg",
+    name: "Jinx and Vi",
   }
 ];
